e4dbe4dd10d3343577d258e1840c3f504426cf0e
inakleinbottle <admin@inakleinbottle.com>
2022-06-14 12:10:50 +0100

    Added trait bounds for serialize and deserialize

--- src/time.rs
+++ src/time.rs
@@ -1,17 +1,17 @@
 
 use std::convert::Into;
-
+use serde::{Serialize, Deserialize};
 
 
 #[repr(transparent)]
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct TimeDelta(i64);
 
 const TIME_RESOLUTION: i64 = 1000;
 
 impl TimeDelta {
 
-    pub fn new(millis: i64) -> TimeDelta { TimeDelta(time) }
+    pub fn new(millis: i64) -> TimeDelta { TimeDelta(millis) }
     pub fn from_secs(secs: i64) -> TimeDelta { TimeDelta(secs * TIME_RESOLUTION) }
 }
 
@@ -36,7 +36,13 @@ impl From<f32> for TimeDelta {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    fn test_create_convert_milis_to_float() {
+        let t = TimeDelta::new(100);
+        let fl: f32 = t.into();
 
+    }
 
 }
