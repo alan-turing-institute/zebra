54dd680e10823ffe450bc8b375b9bcd5a588ef17
Sam Greenbury <50113363+sgreenbury@users.noreply.github.com>
2022-06-20 22:24:01 +0100

    Load simulation settings from config

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,19 @@ struct CLIOptions {}
 fn main() {
     let args = CLIOptions::parse();
 
+    // Get configs
+    let zebra_config = get_zebra_config();
+
     // Load road from config
     let road = Road::config_new();
 
     // Make simulation
     let mut simulation = EventDrivenSim::new(
-        0,0, 60_000,
-        1., 1.,
+        0,
+        0,
+        zebra_config.simulation.run_time,
+        zebra_config.simulation.pedestrian_arrival_rate,
+        zebra_config.simulation.vehicle_arrival_rate,
         Box::new(SimulatorState::new()), 
         road
     );
